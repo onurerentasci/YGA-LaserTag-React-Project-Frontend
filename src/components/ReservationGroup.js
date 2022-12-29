bce2851f792fc92bc78050b8246df4f81840118b
onurerentasci <onurerentasci@gmail.com>
2022-12-30 02:00:47 +0300

    commit

--- src/components/ReservationGroup.js
+++ src/components/ReservationGroup.js
@@ -2,12 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
 import SignUp from "../pages/SignUp";
 import TimeGrid from "./TimeGrid";
+import axios from "axios";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .required("E-Posta alanının doldurulması zorunludur")
+    .email("Invalid email format"),
+  password: Yup.string()
+    .required("Parola alanının doldurulması zorunludur")
+    .min(8, "Parolanız en az 8 karakterden oluşmak zorundadır!"),
+});
 
 export default function ReservationGroup() {
   return (
@@ -15,77 +23,134 @@ export default function ReservationGroup() {
       <div className="row g-0 text-center">
         <div className="col-sm-6 col-md-8 ">
           <h6>Almak istediginiz hizmeti seçiniz!</h6>
-          <FormControl>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel
-                value="LaserTag"
-                control={<Radio />}
-                label="LaserTag"
-              />
-              <FormControlLabel
-                value="PlayStation"
-                control={<Radio />}
-                label="PlayStation"
-              />
-              <FormControlLabel value="PC" control={<Radio />} label="PC" />
-            </RadioGroup>
-          </FormControl>
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { marginTop: 1, width: "25ch" },
+          <Formik
+            initialValues={{
+              picked: "",
             }}
-            noValidate
-            autoComplete="off"
+            onSubmit={async (values) => {
+              await new Promise((r) => setTimeout(r, 500));
+              alert(JSON.stringify(values, null, 2));
+            }}
           >
-            <div className="telefon">
-              <h6>Telefon numaranızı yazınız</h6>
-              <TextField
-                id="outlined-number-input"
-                label="Telefon Numarası"
-                placeholder="53xxxxxxxx"
-              />
-            </div>
-          </Box>
+            {({ values }) => (
+              <Form>
+                <div
+                  className="choice"
+                  role="group"
+                  aria-labelledby="my-radio-group"
+                >
+                  <label>
+                    <Field type="radio" name="choice" value="LaserTag" />
+                    LaserTag
+                  </label>
+                  <label>
+                    <Field type="radio" name="choice" value="Playstation" />
+                    PlayStation
+                  </label>
+                  <label>
+                    <Field type="radio" name="choice" value="PC" />
+                    PC
+                  </label>
+                </div>
+              </Form>
+            )}
+          </Formik>
+          <Formik
+            validationSchema={schema}
+            initialValues={{ phoneNumber: "" }}
+            onSubmit={(values) => {
+              const data = {
+                phoneNumber: values.phoneNumber,
+              };
+
+              const url = "https://localhost:7184/api/user";
+              axios
+                .post(url, data)
+                .then((result) => {
+                  alert(JSON.stringify(result.data));
+                })
+                .catch((error) => {
+                  alert(error);
+                });
+              alert(JSON.stringify(values));
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            }) => (
+              <div className="phoneNumber">
+                <h6>Telefon Numaranızı Yazınız</h6>
+                <form noValidate onSubmit={handleSubmit}>
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.phoneNumber}
+                    placeholder="05xxxxxxxxx"
+                    className="form-control inp_text"
+                    id="phoneNumber"
+                  />
+                  <p className="error">
+                    {errors.phoneNumber &&
+                      touched.phoneNumber &&
+                      errors.phoneNumber}
+                  </p>
+                </form>
+              </div>
+            )}
+          </Formik>
           <div>
-            <FormControl>
-            <h6>Gün seçiniz</h6>
-              <RadioGroup
-                row
-                aria-labelledby="demo-row-radio-buttons-group-label"
-                name="row-radio-buttons-group-date"
-              >
-                <FormControlLabel
-                  value="pazartesi"
-                  control={<Radio />}
-                  label="Pazartesi"
-                />
-                <FormControlLabel
-                  value="sali"
-                  control={<Radio />}
-                  label="Salı"
-                />
-                <FormControlLabel value="carsamba" control={<Radio />} label="Çarşamba" />
-                <FormControlLabel
-                  value="persembe"
-                  control={<Radio />}
-                  label="Perşembe"
-                />
-                <FormControlLabel
-                  value="cuma"
-                  control={<Radio />}
-                  label="Cuma"
-                /><FormControlLabel
-                  value="cumartesi"
-                  control={<Radio />}
-                  label="Cumartesi"
-                />
-              </RadioGroup>
-            </FormControl>
+            <Formik
+              initialValues={{
+                picked: "",
+              }}
+              onSubmit={async (values) => {
+                await new Promise((r) => setTimeout(r, 500));
+                alert(JSON.stringify(values, null, 2));
+              }}
+            >
+              {({ values }) => (
+                <Form>
+                  <h6>Gün seçiniz</h6>
+                  <div
+                    className="choice"
+                    role="group"
+                    aria-labelledby="my-radio-group"
+                  >
+                    <label>
+                      <Field type="radio" name="period" value="Pazartesi" />
+                      Pazartesi
+                    </label>
+                    <label>
+                      <Field type="radio" name="period" value="Sali" />
+                      Salı
+                    </label>
+                    <label>
+                      <Field type="radio" name="period" value="Carsamba" />
+                      Çarşamba
+                    </label>
+                    <label>
+                      <Field type="radio" name="period" value="Persembe" />
+                      Perşembe
+                    </label>
+                    <label>
+                      <Field type="radio" name="period" value="Cuma" />
+                      Cuma
+                    </label>
+                    <label>
+                      <Field type="radio" name="period" value="Cumartesi" />
+                      Cumartesi
+                    </label>
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </div>
           <h6>Saatler</h6>
           <div className="time-grid">
@@ -93,7 +158,7 @@ export default function ReservationGroup() {
           </div>
           <div>
             <div className="submit-button">
-              <button className="button-36" role="button">
+              <button type="submit" className="button-36" role="button">
                 Rezervasyon
               </button>
             </div>
@@ -101,41 +166,73 @@ export default function ReservationGroup() {
         </div>
 
         <div className="col-6 col-md-4">
-          <div className="login">
-            <h3>Giris Yap!</h3>
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "29ch" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <div>
-                <TextField
-                  id="outlined-mail-input"
-                  label="E-Posta"
-                  placeholder="exemple@mail.com"
-                />
-                <TextField
-                  id="outlined-password-input"
-                  label="Parola"
-                  type="password"
-                  autoComplete="current-password"
-                />
+          <Formik
+            validationSchema={schema}
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values) => {
+              const data = {
+                Name: values.email,
+                Fiyat: values.password,
+              };
+
+              const url = "https://localhost:7184/api/user";
+              axios
+                .post(url, data)
+                .then((result) => {
+                  alert(JSON.stringify(result.data));
+                })
+                .catch((error) => {
+                  alert(error);
+                });
+              alert(JSON.stringify(values));
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            }) => (
+              <div className="login">
+                <h3>Giris Yap!</h3>
+                <div className="form">
+                  <form noValidate onSubmit={handleSubmit}>
+                    <input
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      placeholder="E-Posta"
+                      className="form-control inp_text"
+                      id="email"
+                    />
+                    <p className="error">
+                      {errors.email && touched.email && errors.email}
+                    </p>
+                    <input
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                      placeholder="Parola"
+                      className="form-control"
+                    />
+                    <p className="error">
+                      {errors.password && touched.password && errors.password}
+                    </p>
+                    <button type="submit">Giriş Yap</button>
+                  </form>
+                  <a href={"/signup"} element={<SignUp />}>
+                    Üye Değil Misin? Kayıt Ol!
+                  </a>
+                </div>
               </div>
-              <div>
-                <button type="button" className="btn btn-outline-secondary">
-                  Giriş Yap
-                </button>
-              </div>
-              <div>
-                <a href={"/signup"} element={<SignUp />}>
-                  Üye Değil Misin? Kayıt Ol!
-                </a>
-              </div>
-            </Box>
-          </div>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -1,56 +1,142 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import axios from "axios";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  firstName: Yup.string().required("İsim alanının doldurulması zorunludur"),
+  lastName: Yup.string().required("Soyisim alanının doldurulması zorunludur"),
+  eMail: Yup.string()
+    .required("E-Posta alanının doldurulması zorunludur")
+    .email("Invalid email format"),
+  password: Yup.string()
+    .required("Parola alanının doldurulması zorunludur")
+    .min(8, "Parolanız en az 8 karakterden oluşmak zorundadır!"),
+});
 
 export default function SignUpComponent() {
+  const [value, setValue] = useState({
+    firstName: "",
+    lastName: "",
+    eMail: "",
+  });
+
+  useEffect(() => {
+    console.log(value.firstName);
+    console.log(value.lastName);
+    console.log(value.eMail);
+  });
+
+  function handleChange(e) {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
+  }
+
   return (
     <div className="signup-container">
       <div className="row g-0 text-center">
-        <h3>Kayıt Ol!</h3>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
+        <Formik
+          validationSchema={schema}
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
           }}
-          noValidate
-          autoComplete="off"
+          onSubmit={(values) => {
+            const data = {
+              firstName: values.firstName,
+              lastName : value.lastName,
+              eMail: value.eMail,
+              password: value.password
+            };
+
+            const url = "https://localhost:7184/api/user";
+            axios
+              .post(url, data)
+              .then((result) => {
+                alert(JSON.stringify(result.data));
+              })
+              .catch((error) => {
+                alert(error);
+              });
+            alert(JSON.stringify(values));
+          }}
         >
-          <div>
-            <TextField
-              id="outlined-firstname-input"
-              label="Ad"
-              placeholder=""
-              color="secondary"
-            />
-            <TextField
-              id="outlined-lastname-input"
-              label="Soyad"
-              color="secondary"
-              autoComplete="current-password"
-            />
-            <div />
-            <div>
-              <TextField
-                id="outlined-mail-input-signup"
-                label="E-Posta"
-                type="email"
-                color="secondary"
-                placeholder="exemple@domain.com"
-              />
-              <TextField
-                id="outlined-password-input"
-                label="Parola"
-                type="password"
-                color="secondary"
-                autoComplete="current-password"
-              />
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <div className="login">
+              <h3>Kayıt Ol!</h3>
+              <div className="form">
+                <form noValidate onSubmit={handleSubmit}>
+                {/* input ad */}
+                <input
+                    type="firstName"
+                    name="firstName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.firstName}
+                    placeholder="İsim"
+                    className="form-control inp_text"
+                    id="firstName"
+                  />
+                  <p className="error">
+                    {errors.firstName && touched.firstName && errors.firstName}
+                  </p>
+                  {/* İnput soyad */}
+                  <input
+                    type="lastName"
+                    name="lastName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.lastName}
+                    placeholder="Soyisim"
+                    className="form-control inp_text"
+                    id="lastName"
+                  />
+                  <p className="error">
+                    {errors.lastName && touched.lastName && errors.lastName}
+                  </p>
+                  <input
+                    type="eMail"
+                    name="eMail"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.eMail}
+                    placeholder="E-Posta"
+                    className="form-control inp_text"
+                    id="eMail"
+                  />
+                  <p className="error">
+                    {errors.eMail && touched.eMail && errors.eMail}
+                  </p>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    placeholder="Parola"
+                    className="form-control"
+                  />
+                  <p className="error">
+                    {errors.password && touched.password && errors.password}
+                  </p>
+                  <button type="submit">KAYDOL</button>
+                </form>
+              </div>
             </div>
-          </div>
-          <button type="button" className="btn btn-outline-secondary">
-            Kayıt Ol
-          </button>
-        </Box>
+          )}
+        </Formik>
       </div>
     </div>
   );
