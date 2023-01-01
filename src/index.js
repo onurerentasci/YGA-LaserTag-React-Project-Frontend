d32c06a1602477efa7a6ddcbef68cae53e66ee03
Mustafa Karaca <115951479+mustafkrca@users.noreply.github.com>
2023-01-01 15:20:51 +0300

    axios datos kreatos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../src/style/Reservation.css";
+import "../src/style/reservation.css";
 import "./style/Styles.css";
 import { BrowserRouter } from "react-router-dom";
 
